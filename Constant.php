512b33687a6101d3dd51b52fb503b02e7b05e1ab
mahmoud <mahmoud.naseri@diss-co.tech>
2024-02-20 15:26:05 +0330

    modify definition and using constants

--- Constant.php
+++ Constant.php
@@ -2,65 +2,15 @@
 
 class Constant
 {
-    private $tableHeader = ['id', 'Name', 'Max Speed', 'Unit'];
-    private $welcomeMessage = " This is two-player racing game. The game begins with the players choosing a vehicle from a below list \n The length of the race will be 20 km and the vehicle that completes this route the fastest will be the winner of the race \n GoodLuck! \n\n";
-    private $gameName = "----------- Racing Game -----------";
-    private $chooseMessage = " Dear %s, \n Please select one of Vehicles displayed in table abow: ";
-    private $waitingMessage = " Race will be start soon, Joy it...";
-    private $endingRaceMessage = " The competition is over... the result of the competition:";
-    private $wrongInputMessage = " The selected vehicle is not valid! please select a valid one (1-10):";
-    private $showSelectedVehicleMessage = " Nice! Your select number %s that is '%s' With amazing '%s %s' speed! \n\n";
-    private $timeSpendMessage = " Player %s completed the race with vehicle %s in %s minutes ";
-    private $scoreBoardTitleMessage = " \n\n\n----------- SCOREBORD ----------- \n";
-    private $winnerMessage = " \n----------- Player %s is WINNER ----------- \n";
-
-    public function getTableHeader(): array
-    {
-        return $this->tableHeader;
-    }
-
-    public function getWelcomeMessage(): string
-    {
-        return $this->welcomeMessage;
-    }
-
-    public function getGameName(): string
-    {
-        return $this->gameName;
-    }
-
-    public function getChooseMessage(): string
-    {
-        return $this->chooseMessage;
-    }
-
-    public function getWaitingMessage(): string
-    {
-        return $this->waitingMessage;
-    }
-
-    public function getWinnerMessage(): string
-    {
-        return $this->winnerMessage;
-    }
-
-    public function getWrongInputMessage(): string
-    {
-        return $this->wrongInputMessage;
-    }
-
-    public function getShowSelectedVehicleMessage(): string
-    {
-        return $this->showSelectedVehicleMessage;
-    }
-
-    public function getTimeSpendMessage(): string
-    {
-        return $this->timeSpendMessage;
-    }
-
-    public function getScoreBoardTitleMessage(): string
-    {
-        return $this->scoreBoardTitleMessage;
-    }
+    const TABLE_HEADER = ['id', 'Name', 'Max Speed', 'Unit'];
+    const WELCOME_MESSAGE = " This is two-player racing game. The game begins with the players choosing a vehicle from a below list \n The length of the race will be 20 km and the vehicle that completes this route the fastest will be the winner of the race \n GoodLuck! \n\n";
+    const GAME_NAME = "----------- Racing Game -----------";
+    const CHOOSE_MESSAGE = " Dear %s, \n Please select one of Vehicles displayed in table abow: ";
+    const WAITING_MESSAGE = " Race will be start soon, Joy it...";
+    const ENDING_RACE_MESSAGE = " The competition is over... the result of the competition:";
+    const WRONG_INPUT_MESSAGE = " The selected vehicle is not valid! please select a valid one (1-10):";
+    const SHOW_SELECTED_VEHICLE_MESSAGE = " Nice! Your select number %s that is '%s' With amazing '%s %s' speed! \n\n";
+    const TIME_SPEND_MESSAGE = " Player %s completed the race with vehicle %s in %s minutes ";
+    const SCOREBOARD_TITLE_MESSAGE = " \n\n\n----------- SCOREBORD ----------- \n";
+    const WINNER_MESSAGE = " \n----------- Player %s is WINNER ----------- \n";
 }
--- Race.php
+++ Race.php
@@ -19,8 +19,7 @@ class Race extends Utility
 
     public function __construct()
     {
-        $this->cli = new CliBase();
-        $this->constant = new Constant();    
+        $this->cli = new CliBase();    
         $this->vehiclesFile = json_decode(file_get_contents('vehicles.json'), true);
         $this->vehiclesArray = $this->prepareJsonData($this->vehiclesFile);
     }
@@ -32,9 +31,9 @@ class Race extends Utility
     public function initial(): void
     {
         $this->cli->newlineOutput('');
-        $this->cli->colorizedOutput($this->constant->getGameName());
+        $this->cli->colorizedOutput(Constant::GAME_NAME);
         $this->cli->newlineOutput('');
-        $this->cli->simpleOutput($this->constant->getWelcomeMessage());
+        $this->cli->simpleOutput(Constant::WELCOME_MESSAGE);
         $this->vehiclesList();
     }
 
@@ -44,16 +43,16 @@ class Race extends Utility
     public function getPlayerInput(string $player, $isFirstAttempt = true): void
     {
         if ($isFirstAttempt)
-            $this->cli->newlineOutput($this->constant->getChooseMessage(), ['Player '.$player]);
+            $this->cli->newlineOutput(Constant::CHOOSE_MESSAGE, ['Player '.$player]);
 
         $playerInput = $this->formatUserInputs(fgets(STDIN));
         if ($this->validateUserInput($playerInput)) {            
-            $this->cli->newlineOutput($this->constant->getShowSelectedVehicleMessage(), $this->vehiclesArray[($playerInput-1)]);
+            $this->cli->newlineOutput(Constant::SHOW_SELECTED_VEHICLE_MESSAGE, $this->vehiclesArray[($playerInput-1)]);
             $player == 'one' ? 
                 $this->playerOneChoice = $this->vehiclesFile[$playerInput-1] :
                 $this->playerTwoChoice = $this->vehiclesFile[$playerInput-1];       
         } else {
-            $this->cli->newlineOutput($this->constant->getWrongInputMessage());
+            $this->cli->newlineOutput(Constant::WRONG_INPUT_MESSAGE);
             $this->getPlayerInput($player, false);
         }
     }
@@ -77,12 +76,12 @@ class Race extends Utility
     */
     public function scoreBoard(): void
     {
-        $this->cli->newlineOutput($this->constant->getScoreBoardTitleMessage());
+        $this->cli->newlineOutput(Constant::SCOREBOARD_TITLE_MESSAGE);
 
         // display results
         $this->cli->newlineOutput("\n");
-        $this->cli->newlineOutput($this->constant->getTimeSpendMessage(), ['One', $this->playerOneChoice['name'], $this->playerOneTotalTime]);
-        $this->cli->newlineOutput($this->constant->getTimeSpendMessage(), ['Two', $this->playerTwoChoice['name'], $this->playerTwoTotalTime]);
+        $this->cli->newlineOutput(Constant::TIME_SPEND_MESSAGE, ['One', $this->playerOneChoice['name'], $this->playerOneTotalTime]);
+        $this->cli->newlineOutput(Constant::TIME_SPEND_MESSAGE, ['Two', $this->playerTwoChoice['name'], $this->playerTwoTotalTime]);
 
         //display winner
         if ($this->playerOneTotalTime == $this->playerTwoTotalTime) {
@@ -91,12 +90,12 @@ class Race extends Utility
             $winner = $this->playerOneTotalTime > $this->playerTwoTotalTime ? 'Two' : "One";
         }        
 
-        $this->cli->colorizedOutput($this->constant->getWinnerMessage(), [$winner]);
+        $this->cli->colorizedOutput(Constant::WINNER_MESSAGE, [$winner]);
     }    
 
     private function vehiclesList(): void
     {
-        $header = $this->constant->getTableHeader();        
+        $header = Constant::TABLE_HEADER;        
         $this->cli->showTable($header, $this->vehiclesArray);
     }
 }
